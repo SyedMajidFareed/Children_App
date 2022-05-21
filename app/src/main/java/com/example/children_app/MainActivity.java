3f7eb5039c4aa8add7be53aca7d931e9f13fc783
Majid Fareed <majidtracker15@gmail.com>
2022-05-21 16:24:02 +0500

    repository link working

--- app/src/main/java/com/example/children_app/MainActivity.java
+++ app/src/main/java/com/example/children_app/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.children_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button RepoBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RepoBtn = findViewById(R.id.Repobtn);
+        RepoBtn.setOnClickListener(view -> {
+            String webpage = "https://github.com/SyedMajidFareed/Children_App";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webpage));
+            startActivity(intent);
+        });
     }
+
 }
