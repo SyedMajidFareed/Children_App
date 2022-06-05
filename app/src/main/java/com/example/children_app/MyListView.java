692d7da8aa03838970968337be7e2fcd99e8c00d
Majid Fareed <majidtracker15@gmail.com>
2022-06-05 16:40:07 +0500

    list view as a DataClass implemented
    implemented list view but with a class of data

--- app/src/main/java/com/example/children_app/MyListView.java
+++ app/src/main/java/com/example/children_app/MyListView.java
@@ -1,6 +1,7 @@
 package com.example.children_app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,36 +10,34 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MyListView extends ArrayAdapter
-{
-    private final Activity context;
-    private final String[] maintitle;
-    private final String[] subtitle;
-    private final String[] subtitle1;
-    private final Integer[] imgid;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-    public MyListView(Activity context, String[] maintitle, String[] subtitle, String[] subtitle1, Integer[] imgid)
+import java.util.ArrayList;
+
+class MyListViewAdapter extends ArrayAdapter<DataClass>
+{
+
+    public MyListViewAdapter(Context context, ArrayList<DataClass> myDataClass)
     {
-        super(context, R.layout.customized, maintitle);
-        this.context = context;
-        this.maintitle = maintitle;
-        this.subtitle = subtitle;
-        this.subtitle1 = subtitle1;
-        this.imgid = imgid;
+        super(context, 0, myDataClass);
+
     }
-    public View getView(int position, View view, ViewGroup parent)
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
     {
-            LayoutInflater inflater=context.getLayoutInflater();
-            View singleEntityView=inflater.inflate(R.layout.customized, null,true);
-            TextView Alphabet =  singleEntityView.findViewById(R.id.AlphabetTextView);
-            ImageView imageView =  singleEntityView.findViewById(R.id.imageView7);
-            TextView Digits = singleEntityView.findViewById(R.id.DIgitTextView);
-            TextView Roman = singleEntityView.findViewById(R.id.RomantextView);
-            Alphabet.setText(maintitle[position]);
-            imageView.setImageResource(imgid[position]);
-            Digits.setText(subtitle[position]);
-            Roman.setText(subtitle1[position]);
-            return singleEntityView;
-    };
+        DataClass DClass = getItem(position);
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.customized, parent, false);
+        TextView Alphabet =  convertView.findViewById(R.id.AlphabetTextView);
+        TextView Digit = convertView.findViewById(R.id.DIgitTextView);
+        TextView Roman = convertView.findViewById(R.id.RomantextView);
+        ImageView imageView = convertView.findViewById(R.id.imageView7);
+        Alphabet.setText(DClass.getAlphabet());
+        Digit.setText(DClass.getDigit());
+        Roman.setText(DClass.getRoman());
+        imageView.setImageResource(DClass.getImgId());
+        return convertView;
+    }
+
 
 }
